d9bee650bc6ec65eb2813a4c684db7184c6588ee
Guillermo Andrey Guevara Chinchilla <guevaraandrey@gmail.com>
2021-03-02 21:43:47 -0800

    Initial Commit

---
+++ Homework5Pokedex/Homework5PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework5PokedexApp.swift
+//  Homework5Pokedex
+//
+//  Created by Andrey Guevara on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Homework5PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
